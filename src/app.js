acf251affc0fe2bbd4adaf299235fcfce85901d8
Swapan Shaw <swapan.1.shaw@gmail.com>
2016-12-25 17:22:34 +0530

    added documentation about the methods

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 /**
  * 
  * ArrayUtils library contains utility functions which act on arrayUtils
- * and perform operation
+ * and perform operation like add, addAll, contains, map, reduce etc.
+ * 
+ * arrays is the global object used to access the different mthod
  */
 
 var arrays = arrays || {};
 arrays = (function () {
-
+    var _self = this;
     "use strict";
     /**
      * @param o object
@@ -33,17 +35,28 @@ arrays = (function () {
     }
 
     return {
+        /**
+         * join method help to join the element od=f the array with the separator provided as the input param
+         * @param arr input array
+         * @param separator , the regex character which will be used to join 
+         * @returns return string
+         */
         join: function (arr, separator) {
-            if (isArray(arr)) {
-                return Array.prototype.join.call(arr, separator);
-            } else {
-                throw 'input provide is an type of error';
+            if (!isArray(arr)) {
+                throw new TypeError("input is not type of array");
             }
+            return Array.prototype.join.call(arr, separator);
+            
         },
-
+        /**
+         * map method map the array element with the callbac function
+         * @param arr input array
+         * @param func is the callback function
+         * @param return new array
+         */
         map: function (arr, func) {
             if (!isArray(arr) && !isFunction(func)) {
-                throw "input is not type of array or calback functio is not a type of function";
+                throw new TypeError("input is not type of array or calback function is not a type of function");
             }
 
             for (var i = 0; i < arr.length; i++) {
@@ -52,36 +65,70 @@ arrays = (function () {
 
             return arr;
         },
-
-        reduce: function (arr, func, startIndex, endIndex) {
-            var result = 1;
+        /**
+         * reduce function reduce the array into a value based on the callback function
+         * @param arr is input array
+         * @param memo initial value for the reduce
+         * @param func callback function
+         * @param startIndex start index of the element in the array from which reduce start, startIndex is inclusive
+         * @param endIndex is the end index of the ement in the array upto ehich callbac function act upon, 
+         * endIndex is exclusive 
+         * @returns value after reducing the array 
+         */
+        reduce: function (arr, memo, func, startIndex, endIndex) {
+        
+            startIndex = startIndex === 'undefined' ? 0 : startIndex;
+            endIndex = endIndex === 'undefined' ? arr.length :endIndex;
             if (!isArray(arr) && !isFunction(func)) {
-                throw "input is not type of array or calback functio is not a type of function";
+                throw new TypeError("input is not type of array or calback function is not a type of function");
             }
-            for (var i = 0; i < arr.length; i++) {
-                result = func(result, arr[i]);
+            for (var i = startIndex; i < endIndex; i++) {
+                memo = func(memo, arr[i]);
             }
-            return result;
+            return memo;
         },
-
+        /**
+         * add method add @param value values into array @param arr
+         * @param arr input array
+         * @param value input value to be pushed to @param arr
+         * @returns return resulter @param arr, after pushing @param value to @param arr
+         * 
+         */
         add: function (arr, value) {
             if (!isArray(arr)) {
-                throw "input is not type of array or calback functio is not a type of function";
+                throw new TypeError("input is not type of array");
             }
 
             Array.prototype.push.call(arr, value);
         },
+        /**
+         * addAll method add @param otherArr values into @param arr
+         * @param arr input array
+         * @param otherArr input array
+         * @returns return resulter @param arr, which contains element of itself and from otherArr aswell
+         * 
+         */
         addAll: function (arr, otherArr) {
             if (!isArray(arr) && !isArray(otherArr)) {
-                throw "input is not type of array or other is not a type of arrayt";
+                throw new TypeError("input is not type of array");
             }
             for (var i in otherArr) {
                 Array.prototype.push.call(arr, otherArr[i]);
             }
         },
 
+        /**
+         * contains method check wether the @param value is present in the @param array
+         * @param arr input array
+         * @param value which is check, wether it is present in the @param arr or not
+         * @returns return true if value is present in the @param arr else false
+         * 
+         */
         contains: function (arr, value) {
             var flag = false;
+            if (!isArray(arr)) {
+                throw new TypeError("input is not type of array");
+            }
             for (var i = 0; i < arr.length; i++) {
                 if (arr[i] === value) {
                     flag = true;
@@ -90,11 +137,18 @@ arrays = (function () {
             }
             return flag;
         },
-
+        /**
+         * <p>isEquals method check whether two arrays are equals or not. It basically check the 
+         * content of both the array. It reurns 'true' if ane is mirror copy of another</p>
+         * 
+         * @param arr firsr array
+         * @param otherArr another array
+         * @returns 'true' if arr is mirror copy of otherArr
+         */
         isEqusls: function (arr, otherArr) {
             var flag = true;
             if (!isArray(arr) && !isArray(otherArr)) {
-                throw "input is not type of array or other is not a type of arrayt";
+                throw new TypeError("input is not type of array or other is not a type of arrayt");
             }
             if (arr.length !== otherArr.length) {
                 return false;
@@ -118,7 +172,7 @@ arrays = (function () {
          */
         isSorted: function (arr, type) {
             if (!isArray(arr)) {
-                throw "input is not type of array or other is not a type of arrayt";
+                throw new TypeError("input is not type of array or other is not a type of arrayt");
             }
 
 
@@ -139,9 +193,9 @@ console.log(arrays.map([1, 2, 3], function (x) {
     return x * x;
 }));
 
-console.log(arrays.reduce([1, 2, 3, 5, 6, 7], function (total, x) {
-    return total * x;
-}));
+console.log(arrays.reduce(["swapa", "sonam", "sss", "sss"], "", function (memo, x) {
+    return memo+x;
+}, 0 ,2));
 
 console.log(arrays.isEqusls(arr, arr));
 
